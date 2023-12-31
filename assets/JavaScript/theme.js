1ec3f6b5b5c3c1f9e8f3a79cdd752d1a497f6b59
Elvin <elvinsrkrov@gmail.com>
2023-12-31 14:47:41 +0400

    profile,profile-skeleton,buttons style and light-dark mode functionality is added

--- assets/JavaScript/theme.js
+++ assets/JavaScript/theme.js
@@ -1,2 +1,25 @@
 const html = document.documentElement;
 const isDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+console.log(isDark);
+
+if (sessionStorage.getItem("theme")) {
+  html.dataset.theme = sessionStorage.getItem("theme");
+} else {
+  html.dataset.theme = isDark ? "dark" : "light";
+}
+
+let isPressed = false;
+const changeTheme = function () {
+  isPressed = isPressed ? false : true;
+  this.setAttribute("aria-pressed", isPressed);
+  html.setAttribute(
+    "data-theme",
+    html.dataset.theme === "light" ? "dark" : "light"
+  );
+  sessionStorage.setItem("theme", html.dataset.theme);  
+};
+
+window.addEventListener("load", () => {
+  const themeBtn = document.querySelector(".theme-btn");
+  themeBtn.addEventListener("click", changeTheme);
+});
